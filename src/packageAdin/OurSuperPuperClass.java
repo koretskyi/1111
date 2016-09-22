a1c2280b10abb1ce229fc3d5a00b20d8613dfe23
koretskyi <koretskyi@mail.ru>
2016-09-22 19:55:21 +0200

    Initial commit

---
+++ src/packageAdin/OurSuperPuperClass.java
@@ -0,0 +1,15 @@
+package packageAdin;
+
+/**
+ * Created by Vista on 20.09.16.
+ */
+public class OurSuperPuperClass {
+    public static void main(String[] args) {
+        int a = 13;
+        int b = 4;
+        System.out.println(a%b);
+
+
+    }
+
+}
